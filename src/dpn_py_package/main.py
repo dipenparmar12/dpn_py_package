6674dadd893e5b047ef05d544b9c87d462cc2c28
Dipen parmar <dipensavji@gmail.com>
2024-03-29 17:46:25 +0530

    fix

--- src/dpn_py_package/main.py
+++ src/dpn_py_package/main.py
@@ -4,7 +4,9 @@ def xh_main():
     print('==================> [4]:', __name__, 'Xerohome src/main.py')
 
 def add_one(number):
+    print('==================> [7]: add_one()',number )
     return number + 1
 
-def multiple(number):
-    return number + 1
+def multiple(num, num1=10):
+    print('==================> [11]: multiple()', num, num1)
+    return num * num1
